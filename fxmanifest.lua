ba8b21dafc927f79a7e742a5de0e2880cc5f563b
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-05-16 14:57:46 +0200

    v1

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+client_scripts {
+    '**/*_c.lua',
+}
+
+server_scripts {
+    '**/*_s.lua',
+}
---
+++ shuffle_c.lua
@@ -0,0 +1,40 @@
+local isShuffleDisabled = true
+
+CreateThread(function()
+    while true do
+        Wait(0)
+        local ped = PlayerPedId()
+        local veh = GetVehiclePedIsIn(ped)
+        if veh ~= 0 then
+            if isShuffleDisabled and GetPedInVehicleSeat(veh, 0) == ped and GetIsTaskActive(ped, 165) then
+                ClearPedTasksImmediately(ped)
+                SetPedConfigFlag(ped, 184, true)
+                SetPedIntoVehicle(ped, veh, 0)
+            end
+        else
+            Wait(500)
+        end
+    end
+end)
+
+local function HandleShuffleCommand()
+    local ped = PlayerPedId()
+    local veh = GetVehiclePedIsIn(ped)
+
+    if veh ~= 0 then
+        if GetPedInVehicleSeat(veh, -1) == ped and IsVehicleSeatFree(veh, 0) then
+            TaskShuffleToNextVehicleSeat(ped, veh)
+        elseif isShuffleDisabled then
+            SetTimeout(2000, function()
+                isShuffleDisabled = true
+            end)
+            isShuffleDisabled = false
+            SetPedConfigFlag(ped, 184, false)
+        end
+    end
+end
+
+TriggerEvent('chat:addSuggestion', '/shuff', 'Shuffles to the driver seat if you are the passenger or to the passenger seat if you are driver.')
+TriggerEvent('chat:addSuggestion', '/shuffle', 'Shuffles to the driver seat if you are the passenger or to the passenger seat if you are driver.')
+RegisterCommand("shuff", HandleShuffleCommand)
+RegisterCommand("shuffle", HandleShuffleCommand)
